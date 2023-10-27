2efa52c1c82b828bad68421e3a435cf0c2ad66d4
Fabrizio <46307019+Fabriluc@users.noreply.github.com>
2023-10-27 01:57:25 -0300

    jere te amo

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,11 +2,18 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer class="fixed bottom-0 left-0 w-full bg-orange-500 shadow md:px-6 md:py-8">
-      <div class="container mx-auto flex justify-left items-center">
-        <li>
-          <a href="formulario.html">contáctenos pepeepepepep</a>
-        </li>
+    <footer className="site-footer bg-footerbrown fixed bottom-0 left-0 w-full h-14">
+      <div className="contact-container flex items-center justify-start absolute bottom-0 left-0 text-left w-full h-full">
+        <ul className="list-none">
+          <li>
+            <a
+              href="formulario.html"
+              className="inline-block relative px-4 py-2 bg-lightpeach text-jet rounded-full font-semibold uppercase text-sm border-2 border-lightjet shadow-md hover:scale-105 transition-transform duration-300"
+            >
+              contáctenos
+            </a>
+          </li>
+        </ul>
       </div>
     </footer>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,18 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      footerbrown: "#963413",
+      headerred: "#CD4747",
+      lightpeach: "#FFE6BD",
+      metal: "#565584",
+      tahiti: "#3ab7bf",
+      silver: "#ecebff",
+      "bubble-gum": "#ff77e9",
+      bermuda: "#78dcca",
+    },
   },
   plugins: [],
 };
